49c3149aed9f8e1830e28ccc9da66f3f17395d24
ntu-user <ntu-user@a000117f7d92>
2022-11-18 22:09:11 +0000

    Java Application created

---
+++ soft40051_hj_group/src/main/java/com/mycompany/soft40051_hj_group/Soft40051_hj_group.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.soft40051_hj_group;
+
+/**
+ *
+ * @author ntu-user
+ */
+public class Soft40051_hj_group {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
