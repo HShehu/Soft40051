ca39928fa78ee907a9388333d499e819be69880b
ntu-user <ntu-user@9f0d31ed9db4>
2022-12-22 22:51:40 +0000

    Chunking Implemented Sending to Container started

--- soft400051_hj_local/src/main/java/com/mycompany/soft400051_hj_local/UserProfileController.java
+++ soft400051_hj_local/src/main/java/com/mycompany/soft400051_hj_local/UserProfileController.java
@@ -4,25 +4,128 @@
  */
 package com.mycompany.soft400051_hj_local;
 
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import com.mycompany.soft400051_hj_local.model.FileMethods;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.stage.FileChooser;
 
 /**
  *
  * @author ntu-user
  */
-public class UserProfileController {
+public class UserProfileController implements FileMethods{
     
-    public void UploadFile(){
-    FileChooser selectFile = new FileChooser();
-    File selectedFile = selectFile.showOpenDialog(null);
-    
-    if(selectedFile == null)
-    {
-        
+    @Override
+    public void CreateFile(String fileName, String filecontent) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
+    @Override
+    public void CreateFile(File selectedFile){
+        try{
+            selectedFile.createNewFile();
+        }
+        catch (IOException ex) {
+            Logger.getLogger(FileMethods.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+    }
     
-}
+     public void ChunkFile(File ogFile)
+    {
+        try{
+            byte[] fileBytes = Files.readAllBytes(ogFile.toPath());
+            int sizeOfChunk = Arrays.toString(fileBytes).length()/ 4 ;
+            int start = 0;
+            int counter = 0;
+           
+            List<File> chunks = Arrays.asList(
+               new File("./",UUID.randomUUID().toString()+".txt"),
+               new File("./",UUID.randomUUID().toString()+".txt"),
+               new File("./",UUID.randomUUID().toString()+".txt"),
+               new File("./",UUID.randomUUID().toString()+".txt")
+            );
+            
+            
+            for(File chunk:chunks)
+            {
+                try(FileWriter writer = new FileWriter(chunk.getPath())){
+                    if(counter == 3)
+                    {
+                        int remChunkSize = Arrays.toString(fileBytes).length() - (sizeOfChunk*3);
+                        writer.write(Arrays.toString(fileBytes), start, remChunkSize); 
+ 
+                    }
+                    else
+                    {
+                        
+                        writer.write(Arrays.toString(fileBytes), start, sizeOfChunk);
+                    }
+                    
+                }
+                counter++;
+                start += sizeOfChunk;
+            }
+            
+            
+            chunks.forEach((chunk)->{
+            
+              System.out.println(chunk.getPath());
+                
+            });
+        }
+        catch(IOException ioerr){
+            
+        }
+    }
+    
+      public static void SendFile(List<File> chunks)
+    {
+        for(File chunk:chunks)
+        {
+            
+        }
+        try {
+            JSch jsch = new JSch();
+            jsch.setKnownHosts("~/.ssh/known_hosts");
+            jsch.addIdentity("~/.ssh/id_rsa");
+            Session jschSession = jsch.getSession("root","172.18.0.3");
+            jschSession.connect();
+            
+            ChannelSftp sftp = (ChannelSftp)jschSession.openChannel("sftp");
+            sftp.connect();
+            
+            sftp.put(chunk.getPath(),"test/hi.txt");
+            sftp.exit();
+            
+            System.out.println("Working");
+            
+            sftp.disconnect();
+            jschSession.disconnect();
+        } catch (JSchException | SftpException ex) {
+            Logger.getLogger(FileMethods.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    } 
+    public void UploadFile(){
+        FileChooser selectFile = new FileChooser();
+        File uploadFile = selectFile.showOpenDialog(null);
+      
+        if(uploadFile == null)
+        {
+            return;
+        }
+        ChunkFile(uploadFile);      
+    }
+
+
     
 }
--- soft400051_hj_local/src/main/java/com/mycompany/soft400051_hj_local/model/FileMethods.java
+++ soft400051_hj_local/src/main/java/com/mycompany/soft400051_hj_local/model/FileMethods.java
@@ -5,37 +5,14 @@
 package com.mycompany.soft400051_hj_local.model;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 /**
  *
  * @author ntu-user
  */
-public class FileMethods {
-        public static void CreateFile(String content, String name)
-    {
-        
-        try{
-            File userFile = new File("./"+name.concat(".txt"));
-            if(!(userFile.createNewFile()))
-            {
-                System.out.println("File " +name+ " already exists");
-            }
-            System.out.println("File " +name+ " created");
-            
-            try(FileWriter fileWriter = new FileWriter(userFile); )
-            {
-                fileWriter.write(content);
-            }
-            
-
-        } catch (IOException ex) {
-            Logger.getLogger(FileMethods.class.getName()).log(Level.SEVERE, null, ex);
-        }
-//        
-//       
-    }
+public interface FileMethods {
+        void CreateFile(String fileName, String filecontent);
+        void CreateFile(File uploadFile);
+    
 }
