50c90d09964948835522e78a317b7e97466d9569
ntu-user <ntu-user@9f0d31ed9db4>
2022-12-31 03:14:16 +0000

    Implementing Move function
    COPY NOT DELETING CHUNKS(NO WAY TO cancel copy operation)
    
    Add Stipulation for either one copy or to Iterate the copy
    
    PROBLEM: Dialog Pane Not Available
    PROBLEM: Automatically Generate File Database On App Creation
    PROBLEM: Automatically set Logged in Flag to 0 on App Exit

---
+++ soft400051_hj_local/src/main/java/com/mycompany/soft400051_hj_local/MoveDialog.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.soft400051_hj_local;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author ntu-user
+ */
+public class MoveDialog implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
